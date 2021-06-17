c8b49b5be542bb61a00bb77f3d788af41c6482e7
Álvaro Göede Rivera <alvaro@goede.cl>
2021-06-17 17:39:22 -0400

    responsive

--- src/components/ListProjects.tsx
+++ src/components/ListProjects.tsx
@@ -15,7 +15,7 @@ export function ListProjects({ projects }: { projects: ProjectProps }) {
             className="mb-10 lg:mb-0 lg:w-4/12 cursor-pointer transform transition ease-in-out duration-300 hover:-translate-y-2 hover:shadow-xl rounded-2xl bg-blue-700"
             key={project.fields.slug}
           >
-            <article className="p-8 sm:p-10 rounded-2xl flex flex-col md:flex-row lg:flex-col">
+            <article className="p-8 sm:p-10 rounded-2xl flex flex-col sm:flex-row lg:flex-col">
               <div>
                 <h5 className="font-semibold text-xl sm:text-3xl md:text-4xl text-white mb-4">
                   {title}
@@ -24,7 +24,11 @@ export function ListProjects({ projects }: { projects: ProjectProps }) {
                   Mining case tracking software.
                 </p>
               </div>
-              <GatsbyImage image={image as IGatsbyImageData} alt={title} />
+              <GatsbyImage
+                className="sm:w-1/2 lg:w-auto"
+                image={image as IGatsbyImageData}
+                alt={title}
+              />
             </article>
           </Link>
         )
