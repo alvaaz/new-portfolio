78782734f17fbcb259dc42b37245de207451eea6
Álvaro Goede <alvaro@goede.cl>
2023-10-31 11:34:04 -0300

    experimental

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/edge';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.goede.cl',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  experimental: {
+    assets: true
+  },
+  adapter: vercel({
+    imageService: true,
+  })
 });
