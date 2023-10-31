49a82acfe5b41deb12131ceb419d5c4f5932f894
Álvaro Goede <alvaro@goede.cl>
2023-10-31 10:48:57 -0300

    rss xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,9 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
+		items: projects.map((project) => ({
+			...project.data,
+			link: `/portfolio/${project.slug}/`,
 		})),
 	});
 }
