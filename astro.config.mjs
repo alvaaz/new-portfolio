da9fe38fcc65ef0deb04c4a922c67edf3e100c3e
Álvaro Goede <alvaro@goede.cl>
2023-10-31 11:48:22 -0300

    without serveless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://www.goede.cl',
-  integrations: [mdx(), sitemap(), tailwind()],
-  adapter: vercel({
-    imageService: true,
-  })
+  integrations: [mdx(), sitemap(), tailwind()]
 });
